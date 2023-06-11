df473a394f422bf5cd535b22d23fb45281f686d9
路人 <70009473+allcdn@users.noreply.github.com>
2023-06-11 09:05:48 +0800

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+
 sed -i '/^# End of file/,$d' /etc/security/limits.conf
 
 cat >> /etc/security/limits.conf <<EOF
